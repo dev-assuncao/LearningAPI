a0478c98b3683f94191d0e2ae461ee8cf00fddff
dev-assuncao <davisson.assuncao@protonmail.com>
2023-01-18 19:05:25 -0300

    Adicionando ao git relatórios de estudos iniciados na seg.

---
+++ MinhaPrimeiraAPI/MinhaPrimeiraAPI/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> ObterTodos()
+        {
+            return new string[] { "value1", "value2", "value3","value4", "value5" };
+        }
+
+
+        [HttpGet("obter-valores")]
+        public IEnumerable<string> ObterValores()
+        {
+
+            var valores = new string[] { "value1", "value2" };
+
+            if (valores.Length < 5000)
+                return null;
+
+
+            return valores;
+        }
+
+
+        // GET api/values/obter-por-id/5
+        [HttpGet("obter-por-id/{id:int}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
