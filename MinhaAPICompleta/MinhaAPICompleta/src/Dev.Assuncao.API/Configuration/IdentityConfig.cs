51a416fc0c919d6e856bc73495022000f0c5a695
dev-assuncao <davisson.assuncao@protonmail.com>
2023-02-14 13:41:43 -0300

    Learning about custom exceptions in Identity

--- MinhaAPICompleta/MinhaAPICompleta/src/Dev.Assuncao.API/Configuration/IdentityConfig.cs
+++ MinhaAPICompleta/MinhaAPICompleta/src/Dev.Assuncao.API/Configuration/IdentityConfig.cs
@@ -5,6 +5,7 @@ using Dev.Assuncao.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Dev.Assuncao.API.Data;
 using Microsoft.AspNetCore.Identity;
+using Dev.Assuncao.API.Extensions;
 
 namespace Dev.Assuncao.API.Configuration
 {
@@ -20,6 +21,7 @@ namespace Dev.Assuncao.API.Configuration
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
                 .AddDefaultTokenProviders();
 
             return services;
